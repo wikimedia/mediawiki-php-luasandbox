18f387bbabd3ccea60ed35ab7deb7d86e7478124
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-24 16:14:05 +0200

    Link the c++ lua library if building for HHVM
    Change-Id: I2ee5ee8b7ebd6607665e24771035f0a6fde9ff36
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- config.cmake
+++ config.cmake
@@ -1,6 +1,12 @@
-INCLUDE(FindLua51)
+# HHVM needs to link luasandbox to the c++ version of liblua
+if (LUA_USE_CPP)
+  INCLUDE("FindLua51cpp.cmake")
+else(!LUA_USE_CPP)
+  INCLUDE(FindLua51)
+endif(LUA_USE_CPP)
+
 if (!LUA51_FOUND)
-	message(FATAL_ERROR "unable to find Lua 5.1")
+  message(FATAL_ERROR "unable to find Lua 5.1")
 endif()
 
 # Parse version string from debian/changelog and use it to generate luasandbox_version.h
@@ -9,12 +15,12 @@ string(REGEX REPLACE "\\s*php-luasandbox \\(([0-9._-]+).*" "\\1" LUASANDBOX_VERS
 file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/luasandbox_version.h "\#define LUASANDBOX_VERSION \"${LUASANDBOX_VERSION}\"\n")
 
 HHVM_COMPAT_EXTENSION(luasandbox
-	alloc.c
-	data_conversion.c
-	library.c
-	luasandbox.c
-	timer.c
-	luasandbox_lstrlib.c)
+  alloc.c
+  data_conversion.c
+  library.c
+  luasandbox.c
+  timer.c
+  luasandbox_lstrlib.c)
 
 HHVM_ADD_INCLUDES(luasandbox ${LUA_INCLUDE_DIR})
 HHVM_LINK_LIBRARIES(luasandbox ${LUA_LIBRARIES})
