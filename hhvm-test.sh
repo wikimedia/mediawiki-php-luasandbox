11909f6be9972196043756bdfbc63833ea39b3c6
Brad Jorsch <bjorsch@wikimedia.org>
2017-02-23 12:45:23 -0500

    Fix HHVM test script
    The HHVM config used by Jenkins sets hhvm.dynamic_extension_path to
    something other than the default '.', which results in the script
    testing the system luasandbox.so instead of the one that was just
    compiled. Avoid that issue by specifying the full path to the
    just-complied luasandbox.so.
    
    Change-Id: I958c0a105db52bf6cd6ffb42bb05c69d707b9dcd

--- hhvm-test.sh
+++ hhvm-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # First run `phpize`, and then ./hhvm-test.sh run-tests.php
 export TEST_PHP_EXECUTABLE=$0
-hhvm -d "hhvm.dynamic_extensions[luasandbox]=luasandbox.so" "$@"
+hhvm -d "hhvm.dynamic_extensions[luasandbox]=$(pwd)/luasandbox.so" "$@"
